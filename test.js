94be28879e205d55599344e36a21ce7b1b6282b6
Brian Woodward <brian.woodward@gmail.com>
2015-01-13 14:43:07 -0500

    Handling array args
    Since loaders can only take in a single argument, template passes multiple arguments as an array in the first parameter when multiple are specified.
    
    Updating to handle multiple (from template-init) and adding tests.

--- test.js
+++ test.js
@@ -21,5 +21,46 @@ describe('loader', function () {
     loader(a).README.should.have.property.base;
     loader(a).README.should.have.property.cwd;
   });
+
+  it('should accept array as first argument', function () {
+    var a = new File({path: 'README.md', contents: null});
+    var opts = {};
+    loader([a, opts]).should.have.property.README;
+    loader([a, opts]).README.should.have.property.contents;
+    loader([a, opts]).README.should.have.property.relative;
+    loader([a, opts]).README.should.have.property.base;
+    loader([a, opts]).README.should.have.property.cwd;
+  });
+
+  it('should use custom renameKey', function () {
+    var a = new File({path: 'README.md', contents: null});
+    var opts = {
+      renameKey: function (fp) {
+        return fp;
+      }
+    };
+    loader([a, opts]).should.have.property['README.md'];
+    loader([a, opts])['README.md'].should.have.property.contents;
+    loader([a, opts])['README.md'].should.have.property.relative;
+    loader([a, opts])['README.md'].should.have.property.base;
+    loader([a, opts])['README.md'].should.have.property.cwd;
+  });
+
+  it('should use custom normalize', function () {
+    var a = new File({path: 'README.md', contents: null});
+    var opts = {
+      normalize: function (file, opts) {
+        file.foo = 'bar';
+        return file;
+      }
+    };
+    loader([a, opts]).should.have.property.README;
+    loader([a, opts]).README.should.have.property.contents;
+    loader([a, opts]).README.should.have.property.relative;
+    loader([a, opts]).README.should.have.property.base;
+    loader([a, opts]).README.should.have.property.cwd;
+    loader([a, opts]).README.should.have.property.foo;
+    loader([a, opts]).README.foo.should.eql('bar');
+  });
 });
 
